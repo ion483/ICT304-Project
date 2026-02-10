2883f8c4285ff50e812e6fbcc22036a34c1c6b02
Edison <yunxiangzhang350@gmail.com>
2026-02-11 02:47:42 +0800

    model prediction added

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import os
-
+import time
 
 weights_path = os.path.join('runs', 'detect', 'DeepTrack_Results', 'Inventory_Model', 'weights', 'best.pt')
 model = None
@@ -23,6 +23,24 @@ else:
         name = 'Inventory_Model'
     )
 
+
+# test case id 2
+test_images_path = os.path.join('Yolo_data', 'test', 'images')
+
+start_time = time.time()
+
+results = model.predict(source=test_images_path, conf=0.55, save=True)
+
+end_time = time.time()
+
+total_images = len(results)
+total_time = (end_time-start_time)
+avg_time = (total_time/total_images) * 1000 # convert to ms
+
+print(f"\n--- Test case id 2 result ---")
+print(f"Total Images Processed: {total_images}")
+print(f"Total Inference Time: {total_time} secs")
+print(f"Average Inference Time: {avg_time:.2f}ms per image")
 
 
 
