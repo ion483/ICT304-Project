682bbd2ab8ea91c307abd3dd0eff6f634e16f4fb
Edison <yunxiangzhang350@gmail.com>
2026-02-11 02:05:16 +0800

    trained or not trained

--- Assignment.py
+++ Assignment.py
@@ -1,17 +1,29 @@
 from ultralytics import YOLO
 import os
 
-model = YOLO('yolo11n.pt')
 
-yaml_path = os.path.join('.', 'Yolo_data', 'data.yaml')
+weights_path = os.path.join('runs', 'detect', 'DeepTrack_Results', 'Inventory_Model', 'weights', 'best.pt')
+model = None
 
-model.train(
-    data = yaml_path,
-    epochs = 50,
-    imgsz = 640,
-    device = 'cpu',
-    project = 'DeepTrack_Results',
-    name = 'Inventory_Model'
-)
+if os.path.exists(weights_path):
+    print("--- Model found ---")
+    model = YOLO(weights_path)
+else:
+    print("--- Model not found. Start training ---")
+    model = YOLO('yolo11n.pt')
+
+    yaml_path = os.path.join('.', 'Yolo_data', 'data.yaml')
+
+    model.train(
+        data = yaml_path,
+        epochs = 50,
+        imgsz = 640,
+        device = 'cpu',
+        project = 'DeepTrack_Results',
+        name = 'Inventory_Model'
+    )
+
+
+
 
 
